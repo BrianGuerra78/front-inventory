b7edb7c266ab704fe6555755d401e176d79db6d4
brianguerra <brianm.guerrag@outlook.es>
2023-03-16 17:04:39 -0600

    primera vercion de front inventario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardRoutingModule } from './modules/dashboard/routes/dashboard-routingh.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: false, useHash: true}),DashboardRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
