eb3468d9e203218ff95c65cdb49b31a6bbf9ca80
brianguerra <brianm.guerrag@outlook.es>
2023-03-21 13:43:04 -0600

    sidenav

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from '../pages/dashboard/dashboard.component';
-import { HomeComponent } from '../components/home/home.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from '../shared/material.module';
 
 
 
@@ -13,7 +15,7 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    SharedModule
   ]
 })
 export class DashboardModule { }
