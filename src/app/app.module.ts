665bf757be0dea9f1309110675fc65c0651af733
brianguerra <brianm.guerrag@outlook.es>
2023-03-28 09:24:40 -0600

    keycloak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,29 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER. NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+function initializeKeycloak(keycloak: KeycloakService){
+  return()=> keycloak.init({
+    config:{
+      url: 'http://localhost:8082/',
+      realm: 'Inventory',
+      clientId: 'angular-client'
+    },
+    initOptions:{
+      onLoad: 'login-required',
+      flow: "standard",
+      silentCheckSsoRedirectUri:
+       window.location.origin + '/assets/silent-check-sso.html'
+    },
+    loadUserProfileAtStartUp: true
+  });
+}
 
 @NgModule({
   declarations: [
@@ -15,9 +33,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     DashboardModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [ {
+    provide: APP_INITIALIZER,
+    useFactory: initializeKeycloak,
+    multi: true,
+    deps: [KeycloakService]
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
