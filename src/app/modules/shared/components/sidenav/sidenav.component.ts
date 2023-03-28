4eb0ce1e8a784b26b6d459fad019afb827e237f0
Brian Guerra <brianm.guerrag@outlook.es>
2023-03-28 12:57:58 -0600

    implementar serrar secion

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-sidenav',
@@ -9,18 +10,25 @@ import { MediaMatcher } from '@angular/cdk/layout';
 export class SidenavComponent implements OnInit {
 
   mobileQuery: MediaQueryList;
+  username: any;
   menuNav = [
     {name: "Home", route: "home", icon:"home"},
     {name: "Categorias", route: "category", icon: "category"},
     {name: "Productos", route: "product", icon: "production_quantity_limits"}
   ];
 
-  constructor(media: MediaMatcher){
+  constructor(media: MediaMatcher, private keycloakService: KeycloakService){
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
   }
 
-  ngOnInit(): void{
+  shouldRun =true;
 
+  ngOnInit(): void{
+    this.username = this.keycloakService.getUsername();
+  }
+
+  logout(){
+    this.keycloakService.logout();
   }
 
 }
